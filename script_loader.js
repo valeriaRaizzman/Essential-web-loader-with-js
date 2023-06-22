31a388e8813ce661e2502151aa9d963bd88ea4ba
Valeria Raizman <132442225+valeriaRaizzman@users.noreply.github.com>
2023-06-22 10:03:43 +0200

    Update script_loader.js

--- script_loader.js
+++ script_loader.js
@@ -19,6 +19,7 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
+// Function for scaling values
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max-in_min) + out_min;
 }
